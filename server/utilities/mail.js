f2793211ae41b8a241128f651e08e09cb7b62906
Zahra Mumtaz <zahramumtaz@MP17-ZahraM-SrSE-396.local>
2022-01-24 00:44:56 +0500

    email configuration added

--- server/utilities/mail.js
+++ server/utilities/mail.js
@@ -1,1 +1,17 @@
 var nodemailer = require('nodemailer');
+require('dotenv').config(); 
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: process.env.MAILER_NAME,
+        pass: process.env.MAILER_PASSWORD
+    }
+});
+
+async function sendEmail({ from, to, subject, html }) {
+    await transporter.sendMail({ from, to, subject, html });
+}
+
+module.exports = sendEmail;
